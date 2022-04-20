3a40c44da0e2537706905249b69bd6f1a61060c8
Kasim Gokcul <90607952+xviaver@users.noreply.github.com>
2022-04-20 19:25:46 +0300

    English Turkish Dictionary

---
+++ dictionary/dictionary.go
@@ -0,0 +1,50 @@
+package For_Range
+
+import (
+	"fmt"
+)
+
+const (
+	book string = "kitap"
+	kagit
+	deftere
+)
+
+func Demo3() {
+
+	var Sozluk = map[string]string{
+		"book":   "kitap",
+		"table":  "masa",
+		"pencil": "kalem",
+		"school": "okul",
+	}
+	var input string
+	var f bool
+	var w string
+	fmt.Println("Cevirmek istediginiz kelimeyi girin")
+	fmt.Scanln(&input)
+
+	// if _, ok := Sozluk[input]; ok {
+	// 	fmt.Println(Sozluk[input])
+	// } else {
+	// 	fmt.Println("Karsilik bulunamadi")
+	// }
+
+	for k, v := range Sozluk {
+		if input == k {
+			f = true
+			w = v
+			break
+		} else if input == v {
+			f = true
+			w = k
+			break
+		}
+	}
+	if f {
+		fmt.Println(w)
+	} else {
+		fmt.Println("Karsilik Bulunamadi")
+	}
+
+}
