a4da9bbf8e074dc34beb8bbb80ad5e1161ed164b
Kasim Gokcul <90607952+xviaver@users.noreply.github.com>
2022-04-22 18:59:54 +0300

    interface functions added

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	errorhandling "Dictionary/ErrorHandling"
 	numberguessing "Dictionary/NumberGuessing"
 	"Dictionary/dictionary"
+	"Dictionary/interfaces"
 )
 
 func main() {
 	dictionary.TheWords()
 	numberguessing.Guessing()
 	errorhandling.Error()
+	interfaces.Demo2()
 }
